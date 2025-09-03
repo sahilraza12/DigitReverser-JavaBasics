a5ea15bd89a8c8a4ecc969f9764d48e98e24c0f3
SAHIL RAJA <sraza1692@gmail.com>
2025-09-03 15:07:33 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        int n = 10899;
+
+        while(n>0){
+            int LastDigit = n%10;
+            System.out.print(LastDigit + " ");
+             n = n/10;
+        }
+        System.out.println();
+    }
+}
